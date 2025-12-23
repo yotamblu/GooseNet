17756edd216c788b8ca57a4f59a24a2a839b437b
yotam blu <yotamblu@gmail.com>
2025-12-24 00:25:55 +0200

    feat:allowing GooseNet as a PWA

---
+++ GooseNet/service-worker.js
@@ -0,0 +1,10 @@
+﻿self.addEventListener("install", () => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+    self.clients.claim();
+});
+
+// Online-first: no caching yet (safe)
+self.addEventListener("fetch", () => { });
